3b999ac66fa8d5dce82437a73e6e0b4042b88294
KalistoSphinx <shouryadeshmukh6265@gmail.com>
2025-10-29 23:03:14 +0530

    changed some logic

--- main.cpp
+++ main.cpp
@@ -5,9 +5,8 @@ using namespace std;
 
 int V;
 vector<vector<int>> graph;
-vector<vector<int>> allColors; 
+vector<vector<int>> allColorings;
 int minColors = INT_MAX;
-
 
 bool isSafe(int v, vector<int> &color, int c) {
     for (int i = 0; i < V; i++) {
@@ -17,26 +16,22 @@ bool isSafe(int v, vector<int> &color, int c) {
     return true;
 }
 
-
-void graphColoring(int v, vector<int> &color, int m) {
-    if (v == V) { 
-        allColors.push_back(color);
-
+void graphColoringUtil(int v, vector<int> &color, int m) {
+    if (v == V) {
+        allColorings.push_back(color);
         int usedColors = 0;
         vector<bool> used(m + 1, false);
         for (int c : color)
             used[c] = true;
         for (bool u : used)
             if (u) usedColors++;
-
         minColors = min(minColors, usedColors);
         return;
     }
-
     for (int c = 1; c <= m; c++) {
         if (isSafe(v, color, c)) {
             color[v] = c;
-            graphColoring(v + 1, color, m);
+            graphColoringUtil(v + 1, color, m);
             color[v] = 0;
         }
     }
@@ -44,7 +39,7 @@ void graphColoring(int v, vector<int> &color, int m) {
 
 void graphColoring(int m) {
     vector<int> color(V, 0);
-    graphColoring(0, color, m);
+    graphColoringUtil(0, color, m);
 }
 
 int main() {
@@ -53,33 +48,29 @@ int main() {
     cin >> V;
     cout << "Enter number of edges: ";
     cin >> E;
-
     graph.assign(V, vector<int>(V, 0));
-
     cout << "Enter edges (u v) between 0 to " << V - 1 << ":\n";
     for (int i = 0; i < E; i++) {
         int u, v;
         cin >> u >> v;
         graph[u][v] = graph[v][u] = 1;
     }
-
     int maxColors;
     cout << "Enter the maximum number of colors to try: ";
     cin >> maxColors;
-
     graphColoring(maxColors);
-
-    if (allColors.empty()) {
+    if (allColorings.empty()) {
         cout << "\nNo valid colorings possible with " << maxColors << " colors.\n";
     } else {
         cout << "\nAll possible valid colorings:\n";
-        for (auto &c : allColors) {
-            for (int col : c)
-                cout << col << " ";
+        int count = 1;
+        for (auto &c : allColorings) {
+            cout << "Coloring " << count++ << ":\n";
+            for (int i = 0; i < V; i++)
+                cout << "Vertex " << i << " -> Color " << c[i] << endl;
             cout << endl;
         }
-        cout << "\nMinimum number of colors required: " << minColors << endl;
+        cout << "Minimum number of colors required: " << minColors << endl;
     }
-
     return 0;
 }
